25c776721ba5fd2091176afa54ef07ef3246b11f
yashashri.bongulwar <yashashri.bongulwar@michelin.com>
2023-02-27 10:37:28 +0530

    namaste js

---
+++ Javascript/Namaste Js Notes/EP14_Callback_Functions/script.js
@@ -0,0 +1,19 @@
+//---------1. What is a callback Functions------------
+
+
+//Synchronous = happens at the same time. 
+//Asynchronous = doesn't happen at the same time.
+
+//------------2. Js is a synchronous and single threaded language------------
+
+//------------3. Blocking the main thread------------
+
+//------------4. Power of callback------------
+
+//------------5. Deep about Event Listener------------
+
+//------------6. Closures Demo with event listener------------
+
+//------------7. Scope demo with Event Listeners------------
+
+//------------8. Garbage collection and remove eventListeners------------
