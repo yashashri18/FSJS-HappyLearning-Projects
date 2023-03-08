22371f956c5d5f87091f525b3af8a727e6205e23
yashashri.bongulwar <yashashri.bongulwar@michelin.com>
2023-03-08 21:02:01 +0530

    js notes

---
+++ Javascript/fundamentals_practise/p1_variables.js
@@ -0,0 +1,30 @@
+//variables = that holds some value in it
+//to  declare a variable we use var,let,const keywords
+
+
+//var is the old method to declare variable
+// var digit1 = 10;
+// var x = 2;
+// var z = digit1 + x;
+// console.log(z);
+
+//from es6 - let and const are adde
+let digit1 = 10;
+let x; //declaration 
+x=2;   //assign value, defined
+let z = digit1 + x;
+
+//let difit1 = 10, x=2; //valid
+
+
+console.log(z);
+
+const p =12;
+//p=13; //error : once the variable is declared as const , we can not change its value
+
+// Rules for naming variables
+
+// 1. can not start with Number
+// 2. case sensitive
+// 3. Can not use reserved words in js - eg - boolean , int , oublic, this, break, etc
+
---
+++ Javascript/fundamentals_practise/p2_operators.js
@@ -0,0 +1,63 @@
+let x;
+x=10;
+
+//Assignment operator
+let y =x;
+console.log(y)
+
+//Arithmatic - Addition , substraction , multiply, divide
+let z;
+z= x+y;
+// z= x-y;
+// z= x*y;
+// z= x/y
+console.log(z);
+
+
+x = x+2 ; //can also be written as x+=2
+x += 2;
+console.log(x);
+
+//Power
+
+z = x ** 2; //x*x
+z = x ** 3; //x * x * x
+
+//Adding string + number = string 
+console.log(5 + "5");
+
+//Increment, Decrement
+let m = 1;
+m++; //m=m+1
+m--; //m=m-1
+
+//Comparision operators 
+
+5==5 //ture
+6 ==5 //false
+5 == '5' //true - it does not check the datatypes
+5 === '5' //false - it checks the datatype
+8!=5 //true
+5!=5 //false
+5!=='5'//true
+5 !== 8 //true
+5>8 //false
+5 < 8 //true
+8<=8 //true
+
+//Logical Operator
+//And - true only if both conditions are true
+(5<10 && 6>1) //true
+(5>10 && 6>1) //false
+//Or - false only if both conditions are false
+(5<10 || 6>1) //true
+(5>10 || 6>1) //true
+//Not 
+!(6==5) //true
+
+
+
+
+
+
+
