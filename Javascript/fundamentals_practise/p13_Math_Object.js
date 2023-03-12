3ea16ae71dbdb78f3b457fe968ffa94557c8fc94
yashashri.bongulwar <yashashri.bongulwar@michelin.com>
2023-03-12 18:47:09 +0530

    js prac

---
+++ Javascript/fundamentals_practise/p13_Math_Object.js
@@ -0,0 +1,31 @@
+//Math is the predefined Object in Js which has some properties and methods
+
+//having multiple properties
+console.log(Math.PI);
+console.log(Math.E);
+
+//methods
+
+var x = 4.7
+console.log(x) //4.7
+console.log(Math.round(x)) //5
+console.log(Math.round(4.2)) //4
+console.log(Math.ceil(x)) //5
+console.log(Math.floor(x)) //4
+
+
+
+console.log(Math.floor(5.76667))//5 -excludes points
+
+//power
+console.log(Math.pow(2,4)) //16
+//square root
+console.log(Math.sqrt(4)) //2
+//min
+console.log(Math.min(3,-2,7,99,-100)) //-100
+//max
+console.log(Math.max(3,-2,7,99,-100)) //99
+
+
+//similarly there are multiple properties and methods
+
---
+++ Javascript/fundamentals_practise/p14_random.js
@@ -0,0 +1,13 @@
+console.log(Math.random()); //generates random value betww=een 0 to 1
+
+//generate number betwwen 0 top 9
+console.log(Math.floor(Math.random()*10)+1);
+
+//generate number betwwen 0 top 10
+console.log(Math.floor(Math.random()*11)+1);
+
+//generate number betwwen 1 top 10
+console.log(Math.floor(Math.random()*10)+1);
+
+//generate number betwwen 15 top 25
+console.log(Math.floor(Math.random() * (25 - 15))+1) //15 included, 25 excluded
